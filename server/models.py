c559f66d21cb440f6f8429ea8a6996e0b838b8df
vrotondo <vrfr91@gmail.com>
2025-05-22 19:23:13 -0400

    Final student commits

--- server/models.py
+++ server/models.py
@@ -11,7 +11,28 @@ class EmailAddress(db.Model):
 
     @validates('email', 'backup_email')
     def validate_email(self, key, address):
+        # check for presence
+        if not address:
+            raise ValueError("Email must be present.")
+        
+        # Check for type
+        if not isinstance(address, str):
+            raise ValueError("Email must be a string.")
+        
         if '@' not in address:
-            raise ValueError("Email must have an '@' in the address")
+            raise ValueError("Email must have an '@' in the address.")
+        
+        # check for duplicate
+        duplicate_email = db.session.query(EmailAddress.id).filter_by(email = address).first()
+        if duplicate_email is not None:
+            raise ValueError("Email must be unique.")
+        
+        # check email not too long
+        if len(address) > 254:
+            raise ValueError("Email is too long.")
+        
+        # reject hotmail and yahoo emails
+        if address.split("@")[1] in ["hotmail.com", "yahoo.com"]:
+            raise ValueError("Email cannot be a hotmail or yahoo address.")
 
         return address
